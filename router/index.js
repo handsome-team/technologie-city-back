de274cecdf4e587e6a3d30c429751199f43a9f64
yuesongbing <2225886219@qq.com>
2020-12-02 10:01:33 +0800

    ysb

--- router/index.js
+++ router/index.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const Login = require("./login/index");
+const Company = require("./company/company");
 
 router.get("/", (req, res) => {
   res.json({
     code: 200,
-    message: "欢迎进入3030",
+    message: "欢迎进入9700",
   });
 });
 
 router.get("/user", Login.getUser);
+router.get("/company", Company.getCompanyData);
+router.get("/company/detail", Company.getCompanyDetailData);
 
 module.exports = router;
